43ebbff54943d72bcb33a6f7af8ea738726379ee
Aljamil1113 <aarazasaljamil@gmail.com>
2021-08-30 18:36:40 +0800

    HTML/CSS methods

--- 05. HTML_CSS Methods/htmlcss.js
+++ 05. HTML_CSS Methods/htmlcss.js
@@ -1,1 +1,17 @@
-console.log("hello htmlcss.js")
+//console.log("hello htmlcss.js")
+
+//01. addClass() method adds one or more class names to the selected elements.
+//02. after() method insert content after selected elements.
+//03. append() method inserts content at the end of the selected element.
+//04. appendTo() method inserts html elements at the end of a selected element.
+//05. attr() sets or returns attributes/values of a selected element.
+//06. before() method insert content before selected items. 
+//07. clone() method makes a copy of a selected elements
+//08. css() method sets or returns one or more style properties for selected elements
+//09. empty() method removes all child nodes and content from selected elements.
+//10. hasClass() method checks if any of the selected elements have a specified class name
+//11. height() method sets or returns the height of selected elements
+//12. html() method sets or returns the content of a selected elements
+//13. prop() method sets or returns properties/values of a selected elements
+//14. remove() method removes the selected elements (including data and events)
+//15. toggleClass() method toggles between adding/removing one or more classes from selected elements.
